8cd779197cbdf77574b18bdf5969da81c7111ca5
Louis F <80892116+louisBerlin@users.noreply.github.com>
2024-04-15 16:45:31 +0200

    Update basics_TS.ts

--- src/basics_TS.ts
+++ src/basics_TS.ts
@@ -1,63 +1,27 @@
-// Step 1: Declare a variable named 'age' of type 'number' and assign a value to it.
-const age : number = 10;
+//Step 1: Create an array of numbers. Use the '.map' function to double all the numbers in the array.
+//
+const listnumber = [1,2,3,4]
+console.log(listnumber.map((n: number) => {return n * 2}))
+// Step 2: Create an array of words. Use the '.filter' function to select only the words that are longer than 5 letters.
+//
+const listString = ["un","deux","trois","quatre"]
+console.log(    listString.filter((s : string)=> s.length>5)         )
 
-// Step 2: Write a 'for' loop that outputs the numbers from 1 to 'age'.
-for(let i = 1;i<=age;i++){
-    console.log("step 2 : "+i)
+
+// Step 3: Create an array of numbers. Use the '.reduce' function to calculate the sum of all the numbers in the array.
+//
+const listnumber2 = [1,2,3,4]
+console.log(listnumber2.reduce(myFunc))
+
+function myFunc(total:number, num:number) {
+    return total + num;
 }
 
-// Step 3: Use an 'if-else' statement to check if 'age' is greater than 18.
-// Display appropriate messages depending on whether the condition is met or not.
-if(age >= 18){
-    console.log("step 3 : you are older than 18.")
+
+// Step 4: Create an array of numbers. Use the '.some' function to check if at least one number is greater than 10.
+const listnumber3 = [100,200,400,3]
+console.log(listnumber3.some(GreaterThen10))
+
+function GreaterThen10(n:number) {
+    return n > 10;
 }
-else {
-    console.log("step 3 : you're younger than 18. ")
-}
-
-// Step 4: Declare a variable named 'score' of type 'number' and assign it the value 0.
-const score : number = 0;
-
-// Step 5: Use an 'if' statement to check if 'score' has a value.
-// Display 'Score is available.' if 'score' has a value other than 0.
-if(score !== 0){console.log("step 5 : Score is available. score : "+score)}
-else {console.log("step 5 : Score isn´t available. score : "+score)}
-
-// Step 6: Use an 'if-else' statement to check if 'score' is evaluated as a 'truthy' value.
-// Display 'Score is evaluated as truthy.' if 'score' is considered 'truthy'.
-if(score){console.log("step 6 : Score is evaluated as truthy.")}
-else {{console.log("step 6 : Score is evaluated as falsy.")}}
-
-//Step 7: Declare a variable named 'username' of type 'string' and assign it the value '' (empty string).
-const username :string = ""
-
-// Step 8: Use an 'if' statement to check if 'username' has a value.
-// Display 'Username is available.' if 'username' has a value.
-if(username !== ""){
-    console.log("step 8 : Username is available. username = "+username)
-}
-else {
-    console.log("step 8 : Username isn´t available. username = "+username)
-}
-
-// Step 9: Use an 'if-else' statement to check if 'username' is evaluated as a 'falsy' value.
-// Display 'Username is evaluated as falsy.' if 'username' is considered 'falsy'.
- if(username){
-    console.log("step 9 : Username is evaluated as falsy.")
-}
-else {
-    console.log("step 9 : Username is evaluated as truthy.")
-}
-
-// Step 10: Declare a variable named 'isAdmin' of type 'boolean' and assign it the value 'false'.
-const isAdmin :boolean = false
-
-// Step 11: Use an 'if-else' statement to check if 'isAdmin' is evaluated as a 'truthy' value.
-// Display 'isAdmin is evaluated as truthy.' if 'isAdmin' is considered 'truthy'.
-if(isAdmin){console.log("step 11 : isAdmin is evaluated as truthy. isAdmin = "+isAdmin)}
-else {console.log("step 11 : isAdmin is evaluated as falsy. isAdmin = "+isAdmin)}
-
-// Step 12: Use an 'if' statement to check if 'isAdmin' is 'false'.
-// Display 'isAdmin is false.' if 'isAdmin' has the value 'false'.
-if(!isAdmin){console.log("step 12 : isAdmin is false.")}
-else {console.log("step 12 : isAdmin is true.")}
